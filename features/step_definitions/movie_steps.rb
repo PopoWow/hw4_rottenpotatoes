c6ede5617bac497f5308cd033682d68c131db5d2
Kyle Kawamura <kylekawa@gmail.com>
2013-02-15 20:54:59 -0800

    hw4 part3-a

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,22 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    #debugger
+    Movie.create(movie)
+  end
+end
+
+Then /the director of "(.*)" should be "(.*)"/ do |title, director|
+  movie = Movie.find_by_title(title)
+  #debugger
+  movie['director'].should == director
+end
+
+# features/step_definitions/some_definitions.rb
+# convenience so you can call debugger from features file.
+Then /^I debug$/ do
+  debugger
+end
